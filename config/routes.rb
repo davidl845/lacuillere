d4ad71da21586b5ea29ab28b550eaac546e2e141
david lienard <davidl845@hotmail.com>
2016-11-23 23:24:31 +0100

    new routes for chef and top restaurants and reviews model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  resources :restaurants do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    collection do
+      get "top" => "restaurants#top"
+    end
+    member do
+      get "chef" => "restaurants#chef"
+    end
+  end
 end
